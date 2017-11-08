114d834c869a2e10c65cb27cb6f394b9ac2edfa6
PinkyWhale <erica_eli@hotmail.com.ar>
2017-11-08 01:26:20 -0300

    prueba de commit

--- formularios.py
+++ formularios.py
@@ -4,17 +4,18 @@ from wtforms.validators import DataRequired,EqualTo,Regexp,Length
 
 #En este archivo .py se encuentran todas las clases utilizadas en el proyecto.
 #Las siguientes clases sirven para validar campos y obtener parametros.
+#Estas funciones posees en el campo de validacion las expresiones regulares. Estas sirven para poder filtrar los caracteres a la hora de prosesarlos.
 class SearchCliente(FlaskForm):
-    parametro = StringField('Escriba el Nombre del Cliente: ', validators=[Length(min=3, max=100, message="Debe ingresar como minimo 3 caracteres"),DataRequired(message="Debe escribir valor")])
+    parametro = StringField('Escriba el Nombre del Cliente: ', validators=[Length(min=3, max=100, message="Debe ingresar como minimo 3 caracteres."),DataRequired(message="Debe escribir valor")])
 
 class SearchProd(FlaskForm):
-    parametro = StringField('Escriba el Nombre del Producto: ', validators=[Length(min=3, max=100, message="Debe ingresar como minimo 3 caracteres"),DataRequired(message="Debe escribir un valor")])
+    parametro = StringField('Escriba el Nombre del Producto: ', validators=[Length(min=3, max=100, message="Debe ingresar como minimo 3 caracteres."),DataRequired(message="Debe escribir un valor")])
 
 class SearchCant(FlaskForm):
-    parametro = StringField('Escriba la Cantidad que Stock que desea buscar: ', validators=[DataRequired(message="Debe escribir un valor"),Regexp(regex="\d+", message="Solo nùmeros enteros por favor")])
+    parametro = StringField('Escriba la Cantidad que Stock que desea buscar: ', validators=[DataRequired(message="Debe escribir un valor."),Regexp(regex="\d+", message="Solo nùmeros enteros por favor")])
 
 class SearchPrecio(FlaskForm):
-    parametro = StringField('Escriba el Precio que busca: ', validators=[DataRequired(message="Debe escribir un valor"),Regexp(regex="^(\d|-)?(\d|,)*\.?\d*$", message="Ingrese un precio valido")])
+    parametro = StringField('Escriba el Precio que busca: ', validators=[DataRequired(message="Debe escribir un valor."),Regexp(regex="^(\d|-)?(\d|,)*\.?\d*$", message="Ingrese un precio valido")])
     
 
 #Clases para validar usuarios y contraseñas.
